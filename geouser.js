915d44217e80ab08175a818a2a1c9a21bc64ddbb
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-08-21 11:15:58 -0300

    A princípio tudo certo. Só tocar bala no tema agora

--- geouser.js
+++ geouser.js
@@ -1,11 +1,26 @@
 jQuery(document).ready(function($){
 
-if (!$('#geouser-map').length)
+var _map_id = 'geouser-map';
+var _map = $('#' + _map_id);
+var _search = $('#geouser-search');
+var _lat = $('#geouser-lat');
+var _lng = $('#geouser-lng');
+
+if (!_map.length)
     return false;
 
+var initial_center;
+if (geouser.lat && geouser.lng) {
+    initial_center = new google.maps.LatLng(geouser.lat, geouser.lng);
+    initial_zoom = 16;
+} else {
+    initial_center = new google.maps.LatLng(geouser.initial_lat, geouser.initial_lng);
+    initial_zoom = 4;
+}
+
 var options = {
-    center: new google.maps.LatLng(-14.235004,-51.92527999999999),
-    zoom: 7,
+    center: initial_center,
+    zoom: initial_zoom,
     maxZoom: 17,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
@@ -14,17 +29,43 @@ var options = {
     zoomControlOptions: {style: google.maps.ZoomControlStyle.SMALL }
 };
 
-map = new google.maps.Map(document.getElementById("geouser-map"), options);
+var map = new google.maps.Map(document.getElementById(_map_id), options);
+var geocoder = new google.maps.Geocoder();
+var marker;
 
-    /*var myPosition = lugares.params['coords'].split(',');
-    myPosition = new google.maps.LatLng(myPosition[0], myPosition[1]);
-    lugares.markers['myLocation'] = new google.maps.Marker({
-        map: lugares.map,
-        title: 'Minha Localização',
-        position: myPosition,
-        icon: catracaIconMe,
-        shadow: catracaIconShadow
+google.maps.event.addDomListener(window, 'load', function(e) {
+    if (!initial_center)
+        return false;
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        position: initial_center
     });
-    bounds.extend(myPosition);*/
+});
+
+google.maps.event.addListener(map, 'click', function(e) {
+    if (marker) {
+        marker.setPosition(e.latLng);
+    } else {
+        marker = new google.maps.Marker({
+            map: map,
+            draggable: true,
+            position: new google.maps.LatLng(e.latLng)
+        });
+        marker.setPosition(e.latLng);
+    }
+    _lat.val(e.latLng.lat());
+    _lng.val(e.latLng.lng());
+});
+
+_search.keyup(function() {
+    geocoder.geocode({'address': $(this).val()}, function(results){
+        if (!results || !results.length)
+            return false;
+        var location = results[0].geometry.location;
+        map.setCenter(location);
+        map.setZoom(16);
+    });
+});
 
 });

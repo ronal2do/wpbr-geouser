f7924234461c8bd6b581b2f40097ef26f63e576c
matheus <contato@matheusgimenez.com.br>
2015-03-26 19:13:58 -0300

    issue #4

--- geouser.php
+++ geouser.php
@@ -40,25 +40,103 @@ add_action( 'show_user_profile', 'geouser_fields' );
 add_action( 'edit_user_profile', 'geouser_fields' );
 
 function geouser_fields( $user ) {
-    $location = array( 'lat' => false, 'lng' => false );
+    $location = array( 'lat' => false, 'lng' => false, 'type' => false );
     if ( $loc = get_user_meta( $user->ID, 'location', true ) ) {
         $location['lat'] = $loc[0];
         $location['lng'] = $loc[1];
+        $location['type'] = $loc[2];
     }
     ?>
-    <h3><?php _e( 'Geolocalization', 'geouser' ); ?></h3>
-    <table class="form-table">
-    <tr>
-    <th><label for="address"><?php _e( 'Pin your location in the map', 'geouser' ); ?></label></th>
-    <td>
-    <p><?php _e( 'Search address', 'geouser' ); ?>:&nbsp;<input type="text" id="geouser-search" class="regular-text" /></p>
-    <div id="geouser-map" style="display:block; width:500px; height: 300px; border: 1px solid #DFDFDF;"></div>
-    <input type="hidden" id="geouser-lat" name="lat" value="<?php echo $location['lat']; ?>" />
-    <input type="hidden" id="geouser-lng" name="lng" value="<?php echo $location['lng']; ?>" />
-    <p class="help"><?php printf( __( 'Map theme will use %s information for your baloon.<br/>Update your %s account to show a full profile.', 'geouser' ), '<a href="http://gravatar.com">Gravatar</a>', '<b>' . $user->user_email . '</b>' ); ?></p>
-    </td>
-    </tr>
-    </table>
+    <?php $map_table = ''; ?>
+    <?php $map_table .= '<h3>'.__( "Geolocalization", "geouser" ).'</h3>'; ?>
+    <?php $map_table .= '<table class="form-table">'; ?>
+    <?php $map_table .= '<tr>';?>
+    <?php $map_table .= '<th><label for="address">'.__( "Pin your location in the map", "geouser" ).'</label></th>';?>
+    <?php $map_table .= '<td>';?>
+    <?php $map_table .= '<p>'.__( "Search address", "geouser" ).'&nbsp;<input type="text" id="geouser-search" class="regular-text" /></p>';?>
+    <?php $map_table .= '<div id="geouser-map" style="display:block; width:500px; height: 300px; border: 1px solid #DFDFDF;"></div>';?>
+    <?php $map_table .= '<input type="hidden" id="geouser-lat" name="lat" value="'.$location["lat"].'" />';?>
+    <?php $map_table .= '<input type="hidden" id="geouser-lng" name="lng" value="'.$location["lng"].'" />';?>
+    <?php $gravatar_url = '<a href="http://gravatar.com">Gravatar</a>';?>
+    <?php $map_table .= '<p class="help">'; ?>
+    <?php $map_table .= sprintf( __( "Map theme will use %s information for your baloon.<br/>Update your %s account to show a full profile.", "geouser" ), $gravatar_url, '<b>' . $user->user_email . '</b>' );?>
+    <?php $map_table .= '</p>';?>
+    <?php $map_table .= '</td>';?>
+    <?php $map_table .= '</tr>';?>
+    <?php $map_table .= '</table>';?>
+    <?php echo apply_filters('geouser_map_table',$map_table); ?>
+
+    <?php $map_table = ''; ?>
+    <?php $map_table .= '<h3>'.__( "Who are you? / Who do you work for?", "geouser" ).'</h3>'; ?>
+    <?php $map_table .= '<table class="form-table">'; ?>
+    <?php $map_table .= '<tr>';?>
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-blue.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>Freelancer</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="freelancer" '.checked("freelancer",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-red.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>WP Bussines</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="wp-bussines" '.checked("wp-bussines",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-green.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>Agency</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="agency" '.checked("agency",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-cyan.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>Other Bussines</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="other-bussines" '.checked("other-bussines",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-purple.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>Universities</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="universities" '.checked("universities",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-orange.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>Government</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="government" '.checked("government",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '<div style="float:left;display:inline-block;padding:10px">';?>
+    <?php $map_table .= '<label>';?>
+    <?php $map_table .= '<img width="32" height="32" src="' . plugins_url( 'images/pins/big-lime.png', __FILE__ ) . '">';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px;padding-top:10px;"></div>'; ?> 
+    <?php $map_table .= __( "<b>NGO</b>", "geouser" ).'</label>';?>
+    <?php $map_table .= '<div style="clear:both;width:100%;height:5px"></div>'; ?> 
+    <?php $map_table .= '<input type="radio" name="geouser-pin" value="ngo" '.checked("ngo",$location["type"],false).' />';?>
+    <?php $map_table .= '</div>';?>
+
+    <?php $map_table .= '</td>';?>
+    <?php $map_table .= '</tr>';?>
+    <?php $map_table .= '</table>';?>
+    <?php echo apply_filters('geouser_map_pins',$map_table); ?>
 <?php }
 
 add_action( 'personal_options_update', 'geouser_save' );
@@ -71,7 +149,8 @@ function geouser_save( $user_id ) {
 
     if ( !empty( $_POST['lat'] ) && floatval( $_POST['lat'] )
         && !empty( $_POST['lng'] ) && floatval( $_POST['lng'] ) ) {
-        update_user_meta( $user_id, 'location', array( $_POST['lat'], $_POST['lng'] ) );
+        $pin = (!empty($_POST['geouser-pin'])) ? $_POST['geouser-pin'] : null;
+        update_user_meta( $user_id, 'location', array( $_POST['lat'], $_POST['lng'], $pin ) );
     }
 
 }
